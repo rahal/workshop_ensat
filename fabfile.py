a8e7ddefd71d4cd12aafe72fff116a9cbd7a6712
rahal <rahal.aboulfeth@gmail.com>
2017-05-05 15:27:14 +0100

    convey target

--- fabfile.py
+++ fabfile.py
@@ -9,3 +9,6 @@ def run_go():
 def run_go_compose():
     localrun("cd server_go && docker build -t workshop_go ." )
     localrun("docker run -it --rm -p 9090:9090 workshop_go")
+
+def convey():
+    localrun("docker-compose up -d" )
